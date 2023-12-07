59c7affc75bbed924e75d6574389cadee8f0c4d1
Rupam-B <rupam.banerje@gmail.com>
2023-12-07 17:53:44 +0530

    lowernav replaced by img

--- src/Components/LowerNav/LowerNavBar.jsx
+++ src/Components/LowerNav/LowerNavBar.jsx
@@ -4,12 +4,16 @@ import './LowerNavStyle.css'
 const LowerNavBar = () => {
   return (
     <div className='LowerNav-main-div'>
-        <div className='Lower-Nav-Sub-div'>
+        {/* <div className='Lower-Nav-Sub-div'>
             <input type="text" />
             <input type="text" />
             <input type="text" />
             <input type="text" />
             <button><i className="fa-solid fa-magnifying-glass"></i> Search</button>
+        </div> */}
+
+        <div className='Lowenav-add-showing-div'>
+          <img src="https://marutisuzukitruevaluecdn2.azureedge.net/-/media/feature/truevaluehub/articleimages/a-word-of-trust/trust-img-4.jpg?modified=20210201132639" alt="" />
         </div>
     </div>
   )
