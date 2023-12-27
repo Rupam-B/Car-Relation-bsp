8e89843c0379fb03a2dc88e63433e35f08951696
Rupam-B <rupam.banerje@gmail.com>
2023-12-27 16:59:12 +0530

    user-profile-added11

--- src/Components/UserIconsExtends/UserReferrals/UserReferals.jsx
+++ src/Components/UserIconsExtends/UserReferrals/UserReferals.jsx
@@ -15,7 +15,7 @@ const UserReferals = () => {
                     <div className='Referal-add-showing-div' >
                       <img src="https://images.unsplash.com/photo-1583121274602-3e2820c69888?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                       <div className='Referal-adds-info-div'>
-                        <h5>2018 Hyundai Grand i10 maste</h5>
+                        <h6>2018 Hyundai Grand i10 maste</h6>
                         <h6>XUV Ultra</h6>
                         <div className='Referal-adds-info-div-sub'>
                           <p>INR 50,000</p>
@@ -35,7 +35,7 @@ const UserReferals = () => {
                     <div className='Referal-add-showing-div' >
                       <img src="https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                       <div className='Referal-adds-info-div'>
-                        <h5>2019 Ferrari x32 i10 maste</h5>
+                        <h6>2019 Ferrari x32 i10 maste</h6>
                         <h6>XUV Ultra</h6>
                         <div className='Referal-adds-info-div-sub'>
                           <p>INR 1,50,000</p>
