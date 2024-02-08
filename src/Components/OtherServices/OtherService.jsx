d29cffde07d26ccdaf096ceaa3255079c4f2eb73
Rupam-B <rupam.banerje@gmail.com>
2024-02-08 17:54:12 +0530

    redirecting logic handling9

--- src/Components/OtherServices/OtherService.jsx
+++ src/Components/OtherServices/OtherService.jsx
@@ -1,100 +1,118 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./OtherServiceStyle.css";
+import BaseURL from '../../apiconfig'
+import axios from 'axios';
 
 const OtherService = () => {
+
+
+  const [userStoreAdds,setUserStoredAdds] = useState()
+  const [userInsuranceStoreAdds,setUserInsuranceStoredAdds] = useState()
+
+//    For Finance Fetch
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BaseURL}/service/finance`, {
+          mode: "no-cors",
+          headers: {
+            Accept: "application/json",
+          },
+        });
+
+        if (response.status >= 200 && response.status < 300) {
+          const data = response.data;
+          if (data) {
+            // console.log(data.data)
+            setUserStoredAdds(data.data);
+          }
+        } else {
+          throw new Error("Network response was not ok");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  //    For Insurance Fetch
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BaseURL}/service/insurance`, {
+          mode: "no-cors",
+          headers: {
+            Accept: "application/json",
+          },
+        });
+
+        if (response.status >= 200 && response.status < 300) {
+          const data = response.data;
+          if (data) {
+            // console.log(data.data)
+            setUserInsuranceStoredAdds(data.data);
+          }
+        } else {
+          throw new Error("Network response was not ok");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+
   return (
     <div className="OtherServices-main-div">
       <div className="Other-services-sub-div">
         <div className="Finance-sector">
-          <h1>Finance</h1>
-          <div className="Finance-sector-sub">
-            <div className="Finance-sec-image-conatainer">
-              <img
-                className="Finance-iamges"
-                src="https://www.shutterstock.com/shutterstock/photos/2140986129/display_1500/stock-vector-banner-design-of-personal-loan-cartoon-style-template-2140986129.jpg"
-                alt=""
-              />
+        <h1>Finance</h1>
+        <div className='Finance-page-main-div'>
+        <div className='Finance-page-sub-div'>
+            <div className='Finance-company-images-div' >
+                <div className='Finance-company-image-showing-div'>
+                  <img src={userStoreAdds&&userStoreAdds.banner_image} alt="" />
+                </div>
             </div>
-            <div className="Finance-sec-text-conatainer">
-              <p>
-                Car finance encompasses various financial products and services
-                facilitating individuals' vehicle purchases. Key components
-                include auto loans, car leasing, and hire purchase. Interest
-                rates, influenced by factors like credit scores, range from low
-                percentages for excellent credit to higher rates for lower
-                scores. Loan terms vary 24 to 84 months, affecting monthly
-                payments and overall costs. Down payments, typically 10-20%, are
-                common. Market trends show increased online options and fintech
-                influence. Regulations ensure fair lending practices. The global
-                car finance market size fluctuates based on economic conditions
-                and regional variations. Keep in mind that specific details may
-                change, and the latest reports should be consulted for
-                up-to-date information.
-              </p>
+            <div className='Finance-company-advertisement-images-div' >
+                <div className='Finance-company-image-showing-div'>
+                  <img src={userStoreAdds&&userStoreAdds.advertisement_image} alt="" />
+                </div>
             </div>
-          </div>
+            <div className='Finance-Documents-Req-div'>
+                <h4>Documents Required</h4>
+                <div dangerouslySetInnerHTML={{ __html: userStoreAdds && userStoreAdds.details }} />
+            </div>
         </div>
-        <hr />
-        <div className="Exchange-sector">
-          <h1>Exchange</h1>
-          <div className="Finance-sector-sub">
-            <div className="Finance-sec-image-conatainer">
-              <img
-                className="Finance-iamges"
-                src="https://www.shutterstock.com/shutterstock/photos/2337967619/display_1500/stock-photo-money-transfer-concept-money-exchange-2337967619.jpg"
-                alt=""
-              />
-            </div>
-            <div className="Finance-sec-text-conatainer">
-              <p>
-                Embark on a seamless journey to your next dream car with our
-                user-friendly car exchange facility. Simplifying the transition
-                from your current vehicle to a new one, our process offers a
-                stress-free experience. Benefit from competitive trade-in values
-                assessed by our expert team, ensuring a fair and transparent
-                valuation. Explore your options conveniently with our online
-                tools, allowing you to estimate trade-in values and browse our
-                diverse inventory from the comfort of your home. Use your
-                trade-in as a negotiation tool during the purchase of your new
-                car and explore financing options tailored to your budget.
-                Consider the potential tax benefits, guided by our experts to
-                provide a comprehensive financial perspective. With a wide range
-                of models available, our car exchange facility empowers you to
-                upgrade effortlessly. Visit our website or contact us today to
-                embark on a rewarding journey towards your next automotive
-                adventure.
-              </p>
-            </div>
-          </div>
+    </div>
         </div>
         <hr />
         <div className="Insurance-sector">
           <h1>Insurance</h1>
-          <div className="Finance-sector-sub">
-            <div className="Finance-sec-image-conatainer">
-              <img
-                className="Finance-iamges"
-                src="https://cdn.vectorstock.com/i/1000x1000/85/03/domestic-insurance-advertising-banner-vector-28208503.webp"
-                alt=""
-              />
+          <div className='Finance-page-main-div'>
+        <div className='Finance-page-sub-div'>
+            <div className='Finance-company-images-div'>
+                <div className='Finance-company-image-showing-div'>
+                  <img src={userInsuranceStoreAdds&&userInsuranceStoreAdds.banner_image} alt="" />
+                </div>
             </div>
-            <div className="Finance-sec-text-conatainer">
-              <p>
-                Drive with confidence and security through our comprehensive car
-                insurance facility. Our offerings are designed to cater to your
-                unique needs, offering customizable coverage options ranging
-                from basic liability to comprehensive plans. We prioritize
-                affordability, providing competitive premium rates without
-                compromising on the quality of coverage. Experience a seamless
-                and efficient claims process, backed by a 24/7 customer support
-                team ready to assist you at any time. Unlock additional benefits
-                and discounts as part of our insurance packages, rewarding safe
-                driving practices and multi-policy engagements. Opt for peace of
-                mind with our optional emergency roadside assistance coverage,
-                ensuring help is just a call away during unexpected incidents.
-              </p>
+            <div className='Finance-company-advertisement-images-div' >
+                <div className='Finance-company-image-showing-div'>
+                  <img src={userInsuranceStoreAdds&&userInsuranceStoreAdds.advertisement_image} alt="" />
+                </div>
             </div>
-          </div>
+            <div className='Finance-Documents-Req-div'>
+                <h4>Documents Required</h4>
+                <div dangerouslySetInnerHTML={{ __html: userInsuranceStoreAdds && userInsuranceStoreAdds.details }} />
+            </div>
+        </div>
+    </div>
         </div>
         <hr />
       </div>
